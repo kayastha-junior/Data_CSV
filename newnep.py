98f948207f79098780ab1b1e576f1ed6b02c090a
Arjun Saud <arundk055@gmail.com>
2019-12-22 14:28:11 +0545

    scrapper
    scrpaed data from 5 companies of NEPAL

---
+++ newnep.py
@@ -0,0 +1,15 @@
+import csv
+import requests
+from bs4 import BeautifulSoup
+f=open('aa.csv','w',newline='')
+writer=csv.writer(f)
+url=requests.get('http://www.nepalstock.com/main/stockwiseprices/index/3/?startDate=2014-01-01&endDate=2019-09-07&stock-symbol=131&_limit=1315').text
+soup=BeautifulSoup(url,'html.parser')
+mtable=soup.find('table',{'class':'table'})
+tdata=mtable.find_all('tr')[1:-1]
+#print(tdata)
+for row in tdata:
+    cols=row.findChildren(recursive=False)
+    cols=[ele.text.strip() for ele in cols]
+    writer.writerow(cols)
+    print(cols)
